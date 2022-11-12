e90f2f45e25c31e9a6300f79e54f1596bfad19f4
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-11-11 22:42:05 -0600

    responsive

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
         clear
         git status
         git add .
-        git commit --date="230 day ago" -m "updated import"
+        git commit --date="230 day ago" -m "responsive"
         git push origin main
         
