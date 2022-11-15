7e23543fc82405daf09f6881604f54210933e0e0
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-11-14 22:43:45 -0600

    readme update

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
         clear
         git status
         git add .
-        git commit --date="227 day ago" -m "second"
+        git commit --date="227 day ago" -m "readme update"
         git push origin main
         
