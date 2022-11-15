e930f0122a7a52de6197d8f9a082c16d35af95a6
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-11-14 22:43:57 -0600

    ugent update

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
         clear
         git status
         git add .
-        git commit --date="227 day ago" -m "readme update"
+        git commit --date="227 day ago" -m "ugent update"
         git push origin main
         
