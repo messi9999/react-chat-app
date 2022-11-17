04a9ab41add38c9dd9666e3b78da18d8c30ce877
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-11-16 22:44:47 -0600

    old command

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
         clear
         git status
         git add .
-        git commit --date="225 day ago" -m "ugent update"
+        git commit --date="225 day ago" -m "old command"
         git push origin main
         
