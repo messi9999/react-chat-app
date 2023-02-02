0a8a5fbbc47f39302a710d796d52105c821096e1
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2023-02-02 08:10:44 -0600

    updated dashboard

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,7 +10,7 @@ export default function SignIn() {
   return (
     <div className='joinOuterContainer'>
       <div className='joinInnerContainer'>
-        <h1 className='heading'>Join us</h1>
+        <h1 className='heading'>Join</h1>
         <div>
           <input
             placeholder='Name'
